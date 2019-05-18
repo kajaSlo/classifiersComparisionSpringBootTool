2b4bc2cc95266aadfb7738907cc9bf11f76a0ccc
kaja <kajaslomska04@gmail.com>
2019-05-18 15:53:59 +0200

    adding file upload on front end and back end

---
+++ fileUploadFront/ClassifiersFront/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+import {
+  FileUploader,
+  FileSelectDirective
+} from "ng2-file-upload/ng2-file-upload";
+
+const URL = "http://localhost:8080/upload";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  title = "app";
+
+  stringFromBackEnd: any;
+
+  public uploader: FileUploader = new FileUploader({
+    url: URL,
+    itemAlias: "file"
+  });
+
+  ngOnInit() {
+    this.uploader.onAfterAddingFile = file => {
+      file.withCredentials = false;
+    };
+    this.uploader.onCompleteItem = (
+      item: any,
+      response: any,
+      status: any,
+      headers: any
+    ) => {
+      this.stringFromBackEnd = response;
+      console.log("ImageUpload:uploaded:", item, status, response);
+      alert("File uploaded successfully");
+    };
+  }
+}
---
+++ springBootClassifiers/src/main/java/com/classifierscomparision/classifierscomparisiontool/ClassifierscomparisiontoolApplication.java
@@ -0,0 +1,41 @@
+package com.classifierscomparision.classifierscomparisiontool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@SpringBootApplication
+@RestController
+public class ClassifierscomparisiontoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassifierscomparisiontoolApplication.class, args);
+    }
+
+    @RequestMapping(value="/upload", method= RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Object> uploadFile (@RequestParam("file") MultipartFile file) {
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get("/home/kaja/Pulpit/PracaMagisterska/testFileUpload/" + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+        }catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Double result = 3.14567789;
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
