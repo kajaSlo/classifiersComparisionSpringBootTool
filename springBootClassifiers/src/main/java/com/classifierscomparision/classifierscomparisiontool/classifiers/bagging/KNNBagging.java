db1359be34202ae584991b79b47b58dc63d216a4
kaja <kajaslomska04@gmail.com>
2019-07-02 23:48:06 +0200

    adding KNN classifier

---
+++ springBootClassifiers/src/main/java/com/classifierscomparision/classifierscomparisiontool/classifiers/bagging/KNNBagging.java
@@ -0,0 +1,88 @@
+package com.classifierscomparision.classifierscomparisiontool.classifiers.bagging;
+
+import com.classifierscomparision.classifierscomparisiontool.classifiers.DefaultDataSupplier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.lazy.IBk;
+import weka.classifiers.meta.Bagging;
+import weka.core.Instances;
+
+
+public class KNNBagging extends Thread implements DefaultDataSupplier {
+
+    private volatile Double F1score;
+    private volatile Double Accuracy;
+    private volatile Double Sensivity;
+    private volatile Double Specificity;
+
+    String datasetDirectory= "";
+
+    public void makeEvaluation(Instances dataset, IBk model, Bagging bagger) throws Exception{
+        Evaluation evaluation = new Evaluation(dataset);
+
+        evaluation.evaluateModel(bagger, dataset);
+
+
+        Double F1Score = evaluation.weightedFMeasure();
+        Double accuracy = evaluation.pctCorrect()/100;
+        Double sensivity = evaluation.weightedRecall();
+        Double specificity = evaluation.weightedTrueNegativeRate();
+
+        Double weightedResult = (F1Score + accuracy + sensivity + specificity)/4;
+
+        this.F1score=F1Score;
+        this.Accuracy=accuracy;
+        this.Sensivity=sensivity;
+        this.Specificity=specificity;
+    }
+
+    public KNNBagging(String datasetDirectory) {
+        this.datasetDirectory = datasetDirectory;
+    }
+
+
+    public Double getF1Score() {
+        return F1score;
+    }
+
+    public Double getAccuracy() {
+        return Accuracy;
+    }
+
+    public Double getSensivity() {
+        return Sensivity;
+    }
+
+    public Double getSpecificity() {
+        return Specificity;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            Instances dataset = getDataset(datasetDirectory);
+
+            System.out.println("Inside LibSVM classifier");
+
+
+            dataset.setClassIndex(dataset.numAttributes()-1);
+
+            Bagging bagger = new Bagging();
+
+            IBk model = new IBk();
+            model.setKNN(3);
+            bagger.setClassifier(model);
+            bagger.setNumIterations(25);
+            bagger.buildClassifier(dataset);
+            model.buildClassifier(dataset);
+
+            makeEvaluation(dataset, model,bagger);
+            System.out.println("\n");
+
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+}
--- springBootClassifiers/src/main/java/com/classifierscomparision/classifierscomparisiontool/services/MethodSupplierService.java
+++ springBootClassifiers/src/main/java/com/classifierscomparision/classifierscomparisiontool/services/MethodSupplierService.java
@@ -1,8 +1,10 @@
 package com.classifierscomparision.classifierscomparisiontool.services;
 
 import com.classifierscomparision.classifierscomparisiontool.classifiers.bagging.DTBagging;
+import com.classifierscomparision.classifierscomparisiontool.classifiers.bagging.KNNBagging;
 import com.classifierscomparision.classifierscomparisiontool.classifiers.bagging.SVMBagging;
 import com.classifierscomparision.classifierscomparisiontool.classifiers.crossValidation.DTCrossValidation;
+import com.classifierscomparision.classifierscomparisiontool.classifiers.crossValidation.KNNCrossValidation;
 import com.classifierscomparision.classifierscomparisiontool.classifiers.crossValidation.SVMCrossValidation;
 import com.classifierscomparision.classifierscomparisiontool.models.Method;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,67 +21,6 @@ public class MethodSupplierService {
     @Autowired
     private MethodService methodService;
 
-//    public List<Method> addMethodsForDataset(Long dataset_id, String fileName) throws InterruptedException {
-////        Method decisionTreeMethod = new Method();
-////
-////        decisionTreeMethod.setMethodName("Decision tree");
-////        decisionTreeMethod.setResult(0.99999);
-////
-////        Method SVMMethod = new Method();
-////        SVMMethod.setMethodName("SVM");
-////        SVMMethod.setResult(0.12345);
-//
-////        Method DTMethod = methodService.addMethod(dataset_id, decisionTreeMethod);
-////
-////        Method sVMMethod = methodService.addMethod(dataset_id, SVMMethod);
-//
-//        String projectDir = new File(System.getProperty("user.dir")).getParentFile().toString();
-//
-//        List<Thread> threads = new ArrayList<>();
-//        DTCassifier decisionTreeClassifier = new DTCassifier(projectDir + "/datasets/CSVDatasets/" + fileName);
-//        threads.add(decisionTreeClassifier);
-//
-//        SVMLibSVM svmClassifier = new SVMLibSVM(projectDir + "/datasets/CSVDatasets/" + fileName);
-//        threads.add(svmClassifier);
-//
-//        for (Thread thread : threads) {
-//            thread.start();
-//            thread.join();
-//        }
-//
-//        double value = decisionTreeClassifier.getResult();
-//        System.out.println("AAAAAAAAAA: " + value);
-//
-//        double value1 = svmClassifier.getResult();
-//        System.out.println("BBBBBB: " + value1);
-//
-//
-//        Method decisionTreeMethod = new Method();
-//
-//        decisionTreeMethod.setMethodName("Decision tree");
-//        decisionTreeMethod.setResult(value);
-//
-//        Method SVMMethod = new Method();
-//        SVMMethod.setMethodName("SVM");
-//        SVMMethod.setResult(value1);
-//
-//        Method DTMethod = methodService.addMethod(dataset_id, decisionTreeMethod);
-//
-//        Method sVMMethod = methodService.addMethod(dataset_id, SVMMethod);
-//
-////        List<Double> resultsList = new ArrayList<>();
-////        resultsList.add(value);
-////        resultsList.add(value1);
-//
-//        List<Method> methodsList = new ArrayList<>();
-//        methodsList.add(DTMethod);
-//        methodsList.add(SVMMethod);
-//        return methodsList;
-//
-//    }
-
-
-
 
     public void addMethodsForDataset(Long dataset_id, String fileName) throws InterruptedException {
 
@@ -87,8 +28,7 @@ public class MethodSupplierService {
         String projectDir = new File(System.getProperty("user.dir")).getParentFile().toString();
 
         List<Thread> threads = new ArrayList<>();
-
-
+        
 
         DTCrossValidation decisionTreeClassifierCrossValidation = new DTCrossValidation(projectDir + "/datasets/CSVDatasets/" + fileName);
         threads.add(decisionTreeClassifierCrossValidation);
@@ -102,6 +42,12 @@ public class MethodSupplierService {
         SVMBagging svmClassifierBagging = new SVMBagging(projectDir + "/datasets/CSVDatasets/" + fileName);
         threads.add(svmClassifierBagging);
 
+        KNNCrossValidation knnClassifierCrossValidation = new KNNCrossValidation(projectDir + "/datasets/CSVDatasets/" + fileName);
+        threads.add(knnClassifierCrossValidation);
+
+        KNNBagging knnClassifierBagging = new KNNBagging(projectDir + "/datasets/CSVDatasets/" + fileName);
+        threads.add(knnClassifierBagging);
+
         for (Thread thread : threads) {
             thread.start();
             thread.join();
@@ -112,7 +58,7 @@ public class MethodSupplierService {
         Double SensivityDTCrossValidation = decisionTreeClassifierCrossValidation.getSensivity();
         Double SpecificityDTCrossValidation = decisionTreeClassifierCrossValidation.getSpecificity();
 
-       Double weightedResultDTCrossValidation = (F1scoreDTCrossValidation + AccuracyDTCrossValidation + SensivityDTCrossValidation + SpecificityDTCrossValidation)/4;
+        Double weightedResultDTCrossValidation = (F1scoreDTCrossValidation + AccuracyDTCrossValidation + SensivityDTCrossValidation + SpecificityDTCrossValidation)/4;
 
 
         Double F1scoreDTBagging= decisionTreeClassifierBagging.getF1Score();
@@ -137,6 +83,21 @@ public class MethodSupplierService {
         Double SpecificitySVMBagging = svmClassifierBagging.getSpecificity();
 
         Double weightedResultSVMBagging = (F1scoreSVMBagging + AccuracySVMBagging + SensivitySVMBagging + SpecificitySVMBagging)/4;
+
+        Double F1scoreKNNCrossValidation = knnClassifierCrossValidation.getF1Score();
+        Double AccuracyKNNCrossValidation = knnClassifierCrossValidation.getAccuracy();
+        Double SensivityKNNCrossValidation = knnClassifierCrossValidation.getSensivity();
+        Double SpecificityKNNCrossValidation = knnClassifierCrossValidation.getSpecificity();
+
+        Double weightedResultKNNCrossValidation = (F1scoreKNNCrossValidation + AccuracyKNNCrossValidation + SensivityKNNCrossValidation + SpecificityKNNCrossValidation)/4;
+
+
+        Double F1scoreKNNBagging= knnClassifierBagging.getF1Score();
+        Double AccuracyKNNBagging = knnClassifierBagging.getAccuracy();
+        Double SensivityKNNBagging = knnClassifierBagging.getSensivity();
+        Double SpecificityKNNBagging = knnClassifierBagging.getSpecificity();
+
+        Double weightedResultKNNBagging = (F1scoreKNNBagging + AccuracyKNNBagging + SensivityKNNBagging + SpecificityKNNBagging)/4;
 
 
         Method decisionTreeMethodCrossValidation = new Method();
@@ -185,15 +146,39 @@ public class MethodSupplierService {
         SVMMethodBagging.setSensivity(SensivitySVMBagging);
         SVMMethodBagging.setSpecificity(SpecificitySVMBagging);
 
+        Method KNNMethodCrossValidation = new Method();
+
+        KNNMethodCrossValidation.setMethodName("K nearest neighbour");
+        KNNMethodCrossValidation.setResult(weightedResultKNNCrossValidation);
+        KNNMethodCrossValidation.setSplitName("CrossValidation");
+        KNNMethodCrossValidation.setF1Score(F1scoreKNNCrossValidation);
+        KNNMethodCrossValidation.setAccuracy(AccuracyKNNCrossValidation);
+        KNNMethodCrossValidation.setSensivity(SensivityKNNCrossValidation);
+        KNNMethodCrossValidation.setSpecificity(SpecificityKNNCrossValidation);
+
+        Method KNNMethodBagging = new Method();
+
+        KNNMethodBagging.setMethodName("K nearest neighbour");
+        KNNMethodBagging.setResult(weightedResultKNNBagging);
+        KNNMethodBagging.setSplitName("Bagging");
+        KNNMethodBagging.setF1Score(F1scoreKNNBagging);
+        KNNMethodBagging.setAccuracy(AccuracyKNNBagging);
+        KNNMethodBagging.setSensivity(SensivityKNNBagging);
+        KNNMethodBagging.setSpecificity(SpecificityKNNBagging);
 
 
-        Method DTMethodCrossVal = methodService.addMethod(dataset_id, decisionTreeMethodCrossValidation);
 
-        Method DTMethodBagging = methodService.addMethod(dataset_id, decisionTreeMethodBagging);
+        methodService.addMethod(dataset_id, decisionTreeMethodCrossValidation);
 
-        Method svmMethodCrossVal = methodService.addMethod(dataset_id, SVMMethodCrossValidation);
+        methodService.addMethod(dataset_id, decisionTreeMethodBagging);
 
-        Method svmMethodBagging = methodService.addMethod(dataset_id, SVMMethodBagging);
+        methodService.addMethod(dataset_id, SVMMethodCrossValidation);
+
+        methodService.addMethod(dataset_id, SVMMethodBagging);
+
+        methodService.addMethod(dataset_id, KNNMethodCrossValidation);
+
+        methodService.addMethod(dataset_id,KNNMethodBagging);
 
 
     }
